7133d2981fcaa5ada856a0787448823f69f5cd9e
nasuda <nasuda@example.com>
2023-07-30 21:42:00 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,47 @@ function Form() {
   );
 }
 
+const ToForm = () => {
+  const [to, setTo] = useState('tanaka');
+  const [message, setMessage] = useState('Hello');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+       <select
+          value={to}
+          onChange={e => setTo(e.target.value)}
+       >
+        <option value="Tanaka">Tanaka</option>
+        <option value="Hayashi">Hayashi</option>
+       </select>
+      </label>
+     <textarea
+       placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+
+  )
+}
+
 export default function App() {
   return (
     <>
       <h1>Hello</h1>
       <Form />
       <Counter />
+      <ToForm />
     </>
   )
 } 
